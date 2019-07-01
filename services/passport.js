e13f56250b23c5abd0d2d9ac22489f54582d3713
MohamedBelalx <hero.armna@gmail.com>
2019-07-02 01:27:06 +0200

    add proxy

--- services/passport.js
+++ services/passport.js
@@ -18,6 +18,7 @@ passport.use(new GoogleStrategy(
         clientID : keys.googleClientID,
         clientSecret : keys.googleClientSecret,
         callbackURL: '/auth/google/callback',
+        proxy:true
     }, 
     (accesToken,refreshToken,profile,done) => {
         console.log('profile info:',profile);
