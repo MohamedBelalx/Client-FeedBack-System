1db618be648d756797f4f16bacb5443a414140ad
MohamedBelalx <hero.armna@gmail.com>
2019-07-04 17:44:51 +0200

    finsh login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,12 +6,19 @@ app.get('/auth/google',passport.authenticate('google',{
     scope: ['profile','email']
 }));
 
-app.get('/auth/google/callback',passport.authenticate('google'));
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req,res)=>{
+        res.redirect('/surveys');
+    }
+);
 
 
 app.get('/api/logout',(req,res)=>{
     req.logout();
-    res.send('U Loged Out');
+    res.redirect('/')
+    
 });
 
 app.get('/api/cuser',(req,res)=>{
