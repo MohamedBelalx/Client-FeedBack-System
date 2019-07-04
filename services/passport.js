ca2e0235a4ce95628308168b310ba8d80c5aa5d6
MohamedBelalx <hero.armna@gmail.com>
2019-07-04 14:38:01 +0200

    finish user outh

--- services/passport.js
+++ services/passport.js
@@ -8,7 +8,7 @@ passport.serializeUser((user,done)=>{
     done(null,user.id);
 });
 
-passport.deserializeUser((id,done)=>{
+ passport.deserializeUser((id,done)=>{
     User.findById(id).then(user=>{
         done(null,user);
     });
